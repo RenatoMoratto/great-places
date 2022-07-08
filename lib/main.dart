1fd27cfe1021312235c1aed951c2842d08a1caec
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-07-08 11:35:57 -0300

    feat: add environment vars

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 import 'screens/places_list_screen.dart';
@@ -6,7 +7,9 @@ import 'screens/place_form_screen.dart';
 import 'utils/app_routes.dart';
 import 'providers/great_places.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
--- lib/utils/location_util.dart
+++ lib/utils/location_util.dart
@@ -1,10 +1,14 @@
-const googleApiKey = 'AIzaSyAip9TXzhbcvsAyFnlNGOV3LrKaTlwRhIs';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LocationUtil {
   static String generateLocationPreviewImage({
     required double latitude,
     required double longitude,
   }) {
+    String googleApiKey = dotenv.get(
+      'GOOGLE_API_KEY',
+      fallback: 'Google API KEY not found',
+    );
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$googleApiKey';
   }
 }
