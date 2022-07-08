b504a14186680efbad5f8d51e33e22fde44cc02f
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-07-08 16:09:25 -0300

    feat: add marker on selected position

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -5,6 +5,7 @@ import '../models/place.dart';
 
 class MapScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
+  final bool isReadOnly;
 
   const MapScreen({
     Key? key,
@@ -12,6 +13,7 @@ class MapScreen extends StatefulWidget {
       latitude: 37.419857,
       longitude: -122.078827,
     ),
+    this.isReadOnly = false,
   }) : super(key: key);
 
   @override
@@ -19,20 +21,37 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedPosition;
+
+  void _selectPosition(LatLng position) {
+    setState(() {
+      _pickedPosition = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final LatLng initialLocation = LatLng(
+      widget.initialLocation.latitude,
+      widget.initialLocation.longitude,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Selecione...'),
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
-          target: LatLng(
-            widget.initialLocation.latitude,
-            widget.initialLocation.longitude,
-          ),
+          target: initialLocation,
           zoom: 13,
         ),
+        onTap: widget.isReadOnly ? null : _selectPosition,
+        markers: {
+          Marker(
+            markerId: const MarkerId('p1'),
+            position: _pickedPosition ?? initialLocation,
+          ),
+        },
       ),
     );
   }
